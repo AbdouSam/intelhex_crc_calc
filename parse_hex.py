eac6b5d1935ef9e930a5554b1e4aa37db9560bc5
Abdou Sam <abdou.sam7@gmail.com>
2020-02-19 10:58:14 +0100

    Fix python3 issue with hex file, optimize code

--- parse_hex.py
+++ parse_hex.py
@@ -1,3 +1,5 @@
+"""Use C shared library to calculate the crc of a hex_file
+"""
 import sys
 from ctypes import *
 import os 
@@ -5,40 +7,38 @@ import os
 PICMZ_PROG_MEM_LEN =   0x1FFFFF
 
 def get_file_path():
-
-  path = os.path.realpath(__file__)
-
-  pathlist = path.split('/')
-
-  # Remove the name of file.
-  pathlist.pop()
+  """ctypes library requires the full path to the shared
+  library.
+  """
+  pathlist = os.path.realpath(__file__).split('/')[1:-1]
 
   path = ['/' + word for word in pathlist]
 
-  path.pop(0)
-  path.append('/')
-
-  path = ''.join(path)
+  path = ''.join(path) + '/'
 
   return path
 
 def calc_crc_hex_file(hex_file):
+  """calculate crc16 of hex file using shared c library
+  """
 
-  #crclib = cdll.LoadLibrary('./hexcrclib.so')  
   crclib = CDLL(get_file_path() + 'hexcrclib.so')
   
-  crc = crclib.get_crc_hexfile(hex_file, PICMZ_PROG_MEM_LEN)  
+  hex_file_p = create_string_buffer(hex_file.encode())
 
-  print("CRC is ", hex(crc))
+  return crclib.get_crc_hexfile(hex_file_p, PICMZ_PROG_MEM_LEN)  
+
 
 def main():
   # Get hex
   try:
     hex_file = str(sys.argv[1])
   except:
-    raise IOError('Hex file does not exist.')
+    raise IOError('Enter a hex file as first argument.')
 
-  calc_crc_hex_file(hex_file)
+  crc = calc_crc_hex_file(hex_file)
+
+  print(hex(crc))
 
 if __name__ == '__main__':
   main()
